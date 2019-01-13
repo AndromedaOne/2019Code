fa68f4508eefb1678d65bee38b55ed74bf329faf
FRC 4905 Development Laptop <williamschilp@gmail.com>
2019-01-13 10:23:45 -0500

    Added EnumeratedButtons

--- src/main/java/frc/robot/commands/TeleOpDrive.java
+++ src/main/java/frc/robot/commands/TeleOpDrive.java
@@ -3,6 +3,7 @@ package frc.robot.commands;
 import frc.robot.Robot;
 
 import frc.robot.utilities.EnumeratedRawAxis;
+import frc.robot.utilities.ButtonsEnumerated;
 import edu.wpi.first.wpilibj.Joystick;
 
 import edu.wpi.first.wpilibj.command.Command;
@@ -27,7 +28,9 @@ public class TeleOpDrive extends Command {
     	Joystick drivecontroller = Robot.driveController;
     	double forwardBackwardStickValue = -EnumeratedRawAxis.getLeftStickVertical(drivecontroller);
     	double rotateStickValue = EnumeratedRawAxis.getRightStickHorizontal(drivecontroller);
-    	Robot.driveTrain.move(forwardBackwardStickValue, rotateStickValue);
+        Robot.driveTrain.move(forwardBackwardStickValue, rotateStickValue);
+        
+        if(Enumerated)
     }
 
     // Make this return true when this Command no longer needs to run execute()
