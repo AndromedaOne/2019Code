926ddff1ea13ea2a7ee506043d4908fa269a204b
Unknown <sean@ayerdoyles.org>
2019-01-13 14:30:24 -0500

    FIXED THE HUGE MISTAKE THAT DANA PUT ONTO MASTER ON PURPOSE(HE IS TRYING TO SABOTAGE THE TEAM)

--- src/main/java/frc/robot/commands/TeleOpDrive.java
+++ src/main/java/frc/robot/commands/TeleOpDrive.java
@@ -30,7 +30,6 @@ public class TeleOpDrive extends Command {
     	double rotateStickValue = EnumeratedRawAxis.getRightStickHorizontal(drivecontroller);
         Robot.driveTrain.move(forwardBackwardStickValue, rotateStickValue);
         
-        if(Enumerated)
     }
 
     // Make this return true when this Command no longer needs to run execute()
