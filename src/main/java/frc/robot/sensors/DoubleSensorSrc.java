cdc399bb60819c45d199cc9d2a69d00bad632b19
Chris McCubbin <themccubbins@gmail.com>
2019-01-13 15:53:02 -0500

    Sensors -> sensors

---
+++ src/main/java/frc/robot/sensors/DoubleSensorSrc.java
@@ -0,0 +1,5 @@
+package frc.robot.sensors;
+
+public abstract class DoubleSensorSrc extends SensorReadingSrc {
+    public abstract Double getReading(); 
+}
---
+++ src/main/java/frc/robot/sensors/NavXGyroSensor.java
@@ -0,0 +1,46 @@
+package frc.robot.sensors;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.SPI;
+
+public class NavXGyroSensor extends SensorBase<DoubleSensorSrc> {
+    AHRS gyro; /* Alternatives:  SPI.Port.kMXP, I2C.Port.kMXP or SerialPort.Port.kUSB */
+    static NavXGyroSensor instance = new NavXGyroSensor();
+    private NavXGyroSensor(){
+        try {
+			/* Communicate w/navX MXP via the MXP SPI Bus. */
+			/* Alternatively: I2C.Port.kMXP, SerialPort.Port.kMXP or SerialPort.Port.kUSB */
+			/*
+			 * See http://navx-mxp.kauailabs.com/guidance/selecting-an-interface/ for
+			 * details.
+			 */
+			gyro = new AHRS(SPI.Port.kMXP);
+			
+			System.out.println("Created NavX instance");
+			// New thread to initialize the initial angle
+
+		} catch (RuntimeException ex) {
+			DriverStation.reportError("Error instantiating navX MXP:  " + ex.getMessage(), true);
+		}
+    }
+
+    public static NavXGyroSensor getInstance() {
+        return instance;
+    }
+
+    public double getZAngle() {
+        return gyro.getAngle();
+    }
+    
+    public DoubleSensorSrc getClosedLoopZSrc() {
+        return new DoubleSensorSrc(){
+        
+            @Override
+            public Double getReading() {
+                return getZAngle();
+            }
+        };
+    }
+}
