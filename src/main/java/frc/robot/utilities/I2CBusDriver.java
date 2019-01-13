4e2fc71487c437e559d643e8d05ba6e4d1fa355e
Dev-Osmium <devosmium@devosmium.xyz>
2019-01-13 10:18:55 -0500

    Add javadoc

--- src/main/java/frc/robot/utilities/I2CBusDriver.java
+++ src/main/java/frc/robot/utilities/I2CBusDriver.java
@@ -4,8 +4,15 @@ import edu.wpi.first.wpilibj.I2C;
 
 public class I2CBusDriver {
     private I2C i2cBus;
-
+    private int mAddress;
+    /**
+     * Creates a new I2C bus with specified parameters
+     * @param isOnboard is the bus using the port on the RIO or the NavX?
+     * @param address The hexadecimal value of the address (0x1E by default)
+     * @author Owen Salter
+     */
     public I2CBusDriver(boolean isOnboard, int address) {
+        mAddress = address;
         if (isOnboard) {
             i2cBus = new I2C(I2C.Port.kOnboard, address);
         } else {
