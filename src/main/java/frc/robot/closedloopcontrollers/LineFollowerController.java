3435be54c2e296b486a68ea14c582e85bc86e922
Frc4905 <AndromedaOneSoftwareTeam@googlegroups.com>
2019-01-17 20:58:23 -0500

    Adds first part of the sensor array LineFollowerController.

--- src/main/java/frc/robot/closedloopcontrollers/LineFollowerController.java
+++ src/main/java/frc/robot/closedloopcontrollers/LineFollowerController.java
@@ -1,6 +1,43 @@
 package frc.robot.closedloopcontrollers;
+import frc.robot.subsystems.DriveTrain;
+import frc.robot.sensors.LineFollowerSensorArray;
 
 public class LineFollowerController implements ClosedLoopControllerBase {
+    private DriveTrain mDriveTrain;
+    private LineFollowerSensorArray mSensor;
+
+    public LineFollowerController (DriveTrain driveTrain, LineFollowerSensorArray lineFollowerSensorArray) {
+    mDriveTrain = driveTrain;
+    mSensor = lineFollowerSensorArray;
+
+    }
+    
+    public void run() {
+        LineFollowerSensorArray.LineFollowArraySensorReading v = getSensorReading();
+        if(v.lineFound = true) {
+            if(v.lineAngle <= -10) {
+                mDriveTrain.move(5.0, 10.0); //5.0 and 10.0 are placeholder numbers
+            }
+        }
+
+
+    }
+
+    public void reset() {
+
+    }
+
+    public void stop() {
+
+    }
+
+    public void initialize() {
+
+    }
+
+    public boolean isDone() {
+        return false;
+    }
 
 
     
