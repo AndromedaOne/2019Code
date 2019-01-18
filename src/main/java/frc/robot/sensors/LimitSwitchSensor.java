4eedadb0463085eca7a62ec4b0b55feb44b7c976
Dev-Osmium <devosmium@devosmium.xyz>
2019-01-17 20:30:25 -0500

    Document LimitSwitchSensor.java

--- src/main/java/frc/robot/sensors/LimitSwitchSensor.java
+++ src/main/java/frc/robot/sensors/LimitSwitchSensor.java
@@ -6,15 +6,26 @@ public class LimitSwitchSensor {
     private DigitalInput limitSwitch;
     private boolean m_reversedPolarity;
 
+    /**
+     * 
+     * @param port Port number for the DIO port
+     * @param reversedPolarity Whether or not the polarity is reversed
+     */
     public LimitSwitchSensor(int port, boolean reversedPolarity){
         limitSwitch = new DigitalInput(port);
         m_reversedPolarity = reversedPolarity;
     }
 
+    /**
+     * @param port Port number for the DIO port
+     */
     public LimitSwitchSensor(int port){
         this(port, false);
     }
 
+    /**
+     * @return Whether or not the limit switch has been triggered
+     */
     public boolean isAtLimit() {
         if(m_reversedPolarity) {
             return !limitSwitch.get();
