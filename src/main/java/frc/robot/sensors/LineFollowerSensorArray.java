ec30fae5fae596d8fd0f9c899ccb0ddbaaf71dcc
Dev-Osmium <devosmium@devosmium.xyz>
2019-01-19 16:23:18 -0500

    Docs for the docs god

--- src/main/java/frc/robot/sensors/LineFollowerSensorArray.java
+++ src/main/java/frc/robot/sensors/LineFollowerSensorArray.java
@@ -41,6 +41,7 @@ public class LineFollowerSensorArray {
         
        
         mI2cBus.readOnly(buffer, 16);
+        // Step through each even-numbered element in the array
         for (int i = 0; i < buffer.length/2; i++) {
             if(buffer[i * 2] >= 0) {
                 dValues[i] = buffer[i * 2];
@@ -53,6 +54,7 @@ public class LineFollowerSensorArray {
 
 
         SmartDashboard.putNumberArray("LineFollowArray", dValues);
+        // Check for whether the line is found
         for (int i = 0; i < dValues.length; i++) {
                 if (dValues[i] >= detectionThreshold) {
                     boolBuf[i] = true;
