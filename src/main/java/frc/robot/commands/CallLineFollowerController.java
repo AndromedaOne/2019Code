c93eae30d27c297b136b000303d75966d3473269
Frc4905 <AndromedaOneSoftwareTeam@googlegroups.com>
2019-01-19 11:59:00 -0500

    Made SmartDashboard button for CallLineFollowerController command

--- src/main/java/frc/robot/commands/CallLineFollowerController.java
+++ src/main/java/frc/robot/commands/CallLineFollowerController.java
@@ -1,23 +1,33 @@
 package frc.robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import frc.robot.closedloopcontrollers.LineFollowerController;
 
 /**
  *
  */
 public class CallLineFollowerController extends Command {
+    private LineFollowerController controller;
     public CallLineFollowerController() {
+        controller = new LineFollowerController(Robot.driveTrain, Robot.lineFollowerSensorArray);
+        requires(Robot.driveTrain);
+
     }
     @Override
     protected void initialize() {
+        controller.initialize();
     }
     @Override
     protected void execute() {
+        controller.run();
     }
     @Override
     protected boolean isFinished() {
+        return controller.isDone();
     }
     @Override
     protected void end() {
+        controller.stop();
     }
     @Override
     protected void interrupted() {
