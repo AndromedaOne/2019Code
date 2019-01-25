37d57c8a06f4ce3b5d0a593852336b3a85918c55
Frc4905 <AndromedaOneSoftwareTeam@googlegroups.com>
2019-01-24 20:53:15 -0500

    Became inspired and added code for the master motor controller

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -2,7 +2,8 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.*;
+import com.ctre.phoenix.motorcontrol.*;
 import com.typesafe.config.Config;
 
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
@@ -19,21 +20,41 @@ public class DriveTrain extends Subsystem {
     public static WPI_TalonSRX driveTrainRightRearTalon;
     public static SpeedControllerGroup driveTrainRightSpeedController;
     public static DifferentialDrive differentialDrive;
+
+    private final int kTimeoutMs = 30;
     
-    
+    // Inspired by https://github.com/CrossTheRoadElec/Phoenix-Examples-Languages/blob/master/Java/VelocityClosedLoop/src/main/java/frc/robot/Robot.java
+    // and 
     private WPI_TalonSRX initTalonMaster(Config driveConf, String motorName) {
         WPI_TalonSRX _talon = new WPI_TalonSRX(driveConf.getInt(motorName));
+       /* Factory Default all hardware to prevent unexpected behaviour */
         _talon.configFactoryDefault();
+
+        /* Config sensor used for Primary PID [Velocity] */
+        _talon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, kTimeoutMs);
+        
+        /**
+		 * Phase sensor accordingly. 
+         * Positive Sensor Reading should match Green (blinking) Leds on Talon
+         */
+        _talon.setSensorPhase(true);
+        
+        /* Config the peak and nominal outputs */
+		_talon.configNominalOutputForward(0, kTimeoutMs);
+		_talon.configNominalOutputReverse(0, kTimeoutMs);
+		_talon.configPeakOutputForward(1, kTimeoutMs);
+		_talon.configPeakOutputReverse(-1, kTimeoutMs);
+
 
         return _talon;
     }
 
     private WPI_TalonSRX initTalonSlave(Config driveConf, String motorName, WPI_TalonSRX master) {
-        WPI_TalonSRX _talon = new WPI_TalonSRX(driveConf.getInt(motorName));
-        _talon.configFactoryDefault();
-        _talon.follow(master);
+        WPI_TalonSRX slaveMotor = new WPI_TalonSRX(driveConf.getInt(motorName));
+        slaveMotor.configFactoryDefault();
+        slaveMotor.follow(master);
 
-        return _talon;
+        return slaveMotor;
     }
 
     @Override
