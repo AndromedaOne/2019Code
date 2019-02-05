fc7ae6e0527b4d569dd475cf3f312aa4074e7e0c
Ethan McFetridge <ethanmcfetridge@gmail.com>
2019-02-04 20:25:29 -0500

    Added base stuff for infrared distance sensor on claw

---
+++ src/main/java/frc/robot/sensors/infrareddistancesensor/RealInfraredDistanceSensor.java
@@ -0,0 +1,8 @@
+package frc.robot.sensors.infrareddistancesensor;
+
+public class RealInfraredDistanceSensor extends InfraredDistanceSensor {
+    double infraredDistance;
+    public double getInfraredDistance() {
+        return infraredDistance;
+    }
+}
