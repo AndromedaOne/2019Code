d854fb2dfe3dc6c7d541bc8909dbf793388fb308
Dev-Osmium <devosmium@devosmium.xyz>
2019-02-09 14:27:43 -0500

    We have become spotless, destroyer of code

--- src/main/java/frc/robot/closedloopcontrollers/MoveArmAndWristSafely.java
+++ src/main/java/frc/robot/closedloopcontrollers/MoveArmAndWristSafely.java
@@ -2,4 +2,41 @@ package frc.robot.closedloopcontrollers;
 
 public class MoveArmAndWristSafely {
 
+  private final int maxWristRot = 1000;
+  private final int maxExtension = 1000;
+
+  /**
+   * @param extensionValue
+   * @param wristRotValue
+   * @param shoulderRotValue
+   */
+  public static void moveArmWristShoulder(double extensionValue, double wristRotValue, double shoulderRotValue) {
+
+  }
+
+  public boolean isLocSafe(double extensionIn, double wristRotDeg, double shoulderRotDeg) {
+
+    if (shoulderRotDeg > 180 || shoulderRotDeg < -180 || extensionIn < 0 || extensionIn > maxExtension
+        || wristRotDeg > maxWristRot || wristRotDeg < -maxWristRot) {
+      return false;
+    } else if (shoulderRotDeg < -150 && extensionIn > maxExtension - 1) {
+      return false;
+    } else if (shoulderRotDeg > 150 && extensionIn > maxExtension - 1) {
+      return false;
+    } else if (extensionIn < 10 && shoulderRotDeg > 53 && shoulderRotDeg < 127) {
+      return false;
+    } else if (extensionIn < 10 && shoulderRotDeg < -53 && shoulderRotDeg > -127) {
+      return false;
+    } else if (shoulderRotDeg < 50 && shoulderRotDeg > -50 && extensionIn < maxExtension - 1) {
+      if (extensionIn > 13 && wristRotDeg < -90 && shoulderRotDeg > 0 && shoulderRotDeg < 15) {
+        return true;
+      } else if (extensionIn > 13 && wristRotDeg > 90 && shoulderRotDeg < 0 && shoulderRotDeg > -15) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return true;
+    }
+  }
 }
