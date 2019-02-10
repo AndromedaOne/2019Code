28a2097159e47921c59d553f47371c3e32304a96
Fl00pin <zacharymalstrom@gmail.com>
2019-02-10 15:02:58 -0500

    merge

--- src/main/java/frc/robot/commands/DriveForward.java
+++ src/main/java/frc/robot/commands/DriveForward.java
@@ -5,28 +5,27 @@ import frc.robot.Robot;
 
 public class DriveForward extends Command {
 
-    public DriveForward() {
-        requires(Robot.driveTrain);
-    }
-
-
-    @Override
-    protected void initialize() {
-    }
-
-    @Override
-  protected void execute() {
-      Robot.driveTrain.move(.5, 0);
+  public DriveForward() {
+    requires(Robot.driveTrain);
   }
 
-    @Override
-    protected boolean isFinished() {
-        return false;
-    }
+  @Override
+  protected void initialize() {
+  }
 
-    @Override
+  @Override
+  protected void execute() {
+    Robot.driveTrain.move(.5, 0, false);
+  }
+
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  @Override
   protected void end() {
-      Robot.driveTrain.move(0, 0);
+    Robot.driveTrain.move(0, 0, false);
   }
 
   @Override
