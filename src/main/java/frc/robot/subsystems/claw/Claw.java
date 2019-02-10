54c5e0cde3ef018c213832946feaa1cf036e1fc4
Dev-Osmium <devosmium@devosmium.xyz>
2019-02-10 13:00:14 -0500

    Add more abstract methods

--- src/main/java/frc/robot/subsystems/claw/Claw.java
+++ src/main/java/frc/robot/subsystems/claw/Claw.java
@@ -5,5 +5,9 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public abstract class Claw extends Subsystem {
     public abstract void openClaw();
 
+    public abstract void stop();
+
+    public abstract void driveIntakeMotors(double speed);
+
     public abstract void closeClaw();
 }
