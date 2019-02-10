b5fead9b9e5f39069e56818e6707b8a807be74dd
Dev-Osmium <devosmium@devosmium.xyz>
2019-02-10 09:38:01 -0500

    Cleanup RealExtendableArmAndWrist.java

--- src/main/java/frc/robot/subsystems/extendablearmandwrist/RealExtendableArmAndWrist.java
+++ src/main/java/frc/robot/subsystems/extendablearmandwrist/RealExtendableArmAndWrist.java
@@ -4,10 +4,8 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.typesafe.config.Config;
 
-import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import frc.robot.Robot;
-import frc.robot.closedloopcontrollers.pidcontrollers.PIDMultiton;
 
 public class RealExtendableArmAndWrist extends ExtendableArmAndWrist {
   private static RealExtendableArmAndWrist instance;
@@ -16,10 +14,7 @@ public class RealExtendableArmAndWrist extends ExtendableArmAndWrist {
   public final WPI_TalonSRX bottomExtendableArmAndWristTalon;
   protected DifferentialDrive differentialDrive;
   // TODO: Add real encoders
-  private PIDSource topArmAndWristEncoder = null;
-  private PIDSource bottomArmAndWristEncoder = null;
-  private PIDMultiton topArmPid;
-  private PIDMultiton bottomArmPid;
+
 
   public WPI_TalonSRX getShoulderJointTalon() {
     return shoulderJointTalon;
