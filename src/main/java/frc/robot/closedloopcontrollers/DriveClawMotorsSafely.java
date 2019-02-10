c312b536e88ac49c3626c050fe5fed23f4831585
Dev-Osmium <devosmium@devosmium.xyz>
2019-02-10 13:31:23 -0500

    Spotless

--- src/main/java/frc/robot/closedloopcontrollers/DriveClawMotorsSafely.java
+++ src/main/java/frc/robot/closedloopcontrollers/DriveClawMotorsSafely.java
@@ -12,15 +12,17 @@ public class DriveClawMotorsSafely {
   static Config conf;
   static InfraredDistanceSensor iSensor;
 
-  /** Drives the claw intake motors safely per the specifications
+  /**
+   * Drives the claw intake motors safely per the specifications
+   * 
    * @param speed The speed to drive the claw at
    */
   public static void DriveClawMotors(double speed) {
     conf = Robot.getConfig();
     if (conf.hasPath("ports.claw.infrared")) {
-          iSensor = new RealInfraredDistanceSensor(conf.getInt("ports.claw.infrared.port"));
+      iSensor = new RealInfraredDistanceSensor(conf.getInt("ports.claw.infrared.port"));
     } else {
-          iSensor = new MockInfraredDistanceSensor();
+      iSensor = new MockInfraredDistanceSensor();
     }
 
     int threshold = conf.getInt("ports.claw.infrared.threshold");
