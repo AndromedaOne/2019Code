e6934d3e16bc992a57fe9a407912fe962c723c90
Dev-Osmium <devosmium@devosmium.xyz>
2019-02-10 13:13:00 -0500

    Finish cleaning up RealClaw.java

--- src/main/java/frc/robot/subsystems/claw/RealClaw.java
+++ src/main/java/frc/robot/subsystems/claw/RealClaw.java
@@ -5,24 +5,51 @@ import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.typesafe.config.Config;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import frc.robot.Robot;
 
 public class RealClaw extends Claw {
     VictorSPX leftGripperSPX;
     VictorSPX rightGripperSPX;
+    DoubleSolenoid jawSolenoid;
 
     public RealClaw() {
         Config conf = Robot.getConfig();
         Config clawConf = conf.getConfig("ports.claw");
         leftGripperSPX = new VictorSPX(clawConf.getInt("leftGripper"));
         rightGripperSPX = new VictorSPX(clawConf.getInt("rightGripper"));
+        jawSolenoid = new DoubleSolenoid(clawConf.getInt("forwardChannel"), clawConf.getInt("backwardsChannel"));
         
     }
-    public void driveGripperMotors(double speed) {
+
+    @Override
+    protected void initDefaultCommand() {
+        //TODO: Create command to teleop this
+    }
+
+    @Override
+    public void openClaw() {
+        jawSolenoid.set(Value.kForward);
+    }
+
+    @Override
+    public void stop() {
+        leftGripperSPX.set(ControlMode.PercentOutput, 0);
+        jawSolenoid.set(Value.kOff);
+    }
+
+    @Override
+    public void driveIntakeMotors(double speed) {
         leftGripperSPX.set(ControlMode.PercentOutput, speed);
         rightGripperSPX.set(ControlMode.PercentOutput, speed);
     }
+
+    @Override
+    public void closeClaw() {
+        jawSolenoid.set(Value.kReverse);
+    }
+
+
 
 
 }
