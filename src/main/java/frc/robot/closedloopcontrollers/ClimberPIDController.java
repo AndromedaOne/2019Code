fdb0d00fce8fdcfb1428f6a307ef1b0b2d3dc054
Dev-Osmium <devosmium@devosmium.xyz>
2019-02-23 10:21:38 -0500

    Add constructor

--- src/main/java/frc/robot/closedloopcontrollers/ClimberPIDController.java
+++ src/main/java/frc/robot/closedloopcontrollers/ClimberPIDController.java
@@ -1,6 +1,14 @@
 package frc.robot.closedloopcontrollers;
 
+import frc.robot.Robot;
+import frc.robot.sensors.ultrasonicsensor.UltrasonicSensor;
+import frc.robot.telemetries.Trace;
+
 public class ClimberPIDController extends PIDControllerBase {
+
+    UltrasonicSensor ultrasonicSensor;
+    private static DrivetrainUltrasonicPIDController instance;
+    private UltrasonicPIDOut ultrasonicPIDOut;
 
     public ClimberPIDController() {
         super.absoluteTolerance = 0;
@@ -9,6 +17,20 @@ public class ClimberPIDController extends PIDControllerBase {
         super.d = 0;
         super.subsystemName = "ClimberUltrasonic";
         super.pidName = "ClimberPID";
+        ultrasonicSensor = Robot.climbUltrasonicSensor;
+        super.trace = Trace.getInstance();
+        ultrasonicSensor.putSensorOnLiveWindow(super.subsystemName, "ultrasonic");
+        ultrasonicPIDOut = new UltrasonicPIDOut();
+        super.setPIDConfiguration(pidConfiguration);
+        super.pidMultiton = PIDMultiton.getInstance(ultrasonicSensor, ultrasonicPIDOut, pidConfiguration);    
+    }
+
+    private class UltrasonicPIDOut implements PIDOutput {
+
+        @Override
+        public void pidWrite(double output) {
+            
+        }
     }
 
 }
