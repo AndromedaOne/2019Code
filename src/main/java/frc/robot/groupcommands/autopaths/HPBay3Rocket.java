896da254be44bce35fd61d005b4962b07b2b3be4
Caleb Callaluca <CJCallaluca12@gmail.com>
2019-02-23 16:48:33 -0500

    Created empty HPBay3Rocket auto

---
+++ src/main/java/frc/robot/groupcommands/autopaths/HPBay3Rocket.java
@@ -0,0 +1,14 @@
+package frc.robot.groupcommands.autopaths;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class HPBay3Rocket extends CommandGroup {
+    /**
+     * Left or Right of lvl 2 to deliver a HP to cargo bay 3. Then goes to
+     * loading station to grab 2nd HP, then delivers it to left or right Rocket
+     */
+    public HPBay3Rocket()    {
+        
+        
+    }
+}
