bbbfad8c711e74401672ed7ce70b58272d2097f6
BineGrid <dcannava02@gmail.com>
2019-02-23 15:13:53 -0500

    Added HPBay3

---
+++ src/main/java/frc/robot/groupcommands/autopaths/HPBay3.java
@@ -0,0 +1,26 @@
+package frc.robot.groupcommands.autopaths;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.commands.*;
+
+public class HPBay3 extends CommandGroup {
+
+    /**
+     * This does not support us being in the middle
+     */
+    public HPBay3(boolean onRightSide, boolean onLevelTwo, boolean useLineSensorTurn) {
+
+        // Initial Move forward, move further for lv 2 because we are further back
+        if(onLevelTwo) {
+            addSequential(new MoveUsingEncoderPID(18));
+            // Fix rotational error after fall from L2
+            addSequential(new TurnToCompassHeading(0));
+            addSequential(new MoveUsingBackUltrasonic(28));
+        } else {
+            addSequential(new MoveUsingBackUltrasonic(28));
+        }
+
+
+    }
+
+}
--- src/main/java/frc/robot/groupcommands/autopaths/L1HPBay3Bay2.java
+++ src/main/java/frc/robot/groupcommands/autopaths/L1HPBay3Bay2.java
@@ -106,7 +106,7 @@ public class L1HPBay3Bay2 extends CommandGroup {
             At this point, we will return to the loading station and obtain the 
             Second Hatch Panel.
             */
-            addSequential(new MoveUsingEncoderPID(0));
+            addSequential(new MoveUsingBackLineFollower());
             //Follow the line backwards.
             addSequential(new TurnToCompassHeading(0));
             //Turn to face the loading station.
@@ -140,7 +140,7 @@ public class L1HPBay3Bay2 extends CommandGroup {
             //Move into Bay 2.
             addSequential(new CloseClaw());
             //Close the claw to release the Hatch Panel.
-            addSequential(new MoveUsingEncoderPID(0));
+            addSequential(new MoveUsingFrontLineFollower());
             //Move to the end of the line.
             addSequential(new LowSameSideGamePieceArmCommand());
             //Move the arm to the front of the robot.
