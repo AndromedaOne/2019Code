caa5ca58c46f41cc5790475b5d9f7e96e942c3cf
BineGrid <dcannava02@gmail.com>
2019-02-23 16:21:43 -0500

    Added AutoStartingConfig

---
+++ src/main/java/frc/robot/groupcommands/autopaths/AutoStartingConfig.java
@@ -0,0 +1,8 @@
+package frc.robot.groupcommands.autopaths;
+
+public class AutoStartingConfig {
+    public boolean inMiddle;
+    public boolean onRightSide;
+    public boolean onLevelTwo;
+    public boolean useLineSensorTurn;
+}
