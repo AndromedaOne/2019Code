d355d7a378290031aae6ae3b3f9da7663a9f0ba4
BineGrid <dcannava02@gmail.com>
2019-02-23 13:20:20 -0500

    WIP Stuff

--- src/main/java/frc/robot/groupcommands/autopaths/L1MHPCenterCenter.java
+++ src/main/java/frc/robot/groupcommands/autopaths/L1MHPCenterCenter.java
@@ -2,12 +2,36 @@ package frc.robot.groupcommands.autopaths;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.commands.*;
-import frc.robot.Robot;
+import frc.robot.groupcommands.armwristcommands.LowGamePieceArmCommand;
+
 
 public class L1MHPCenterCenter extends CommandGroup {
     private boolean isDone = false;
 
-    public L1MHPCenterCenter(boolean turnRight) {
+    public L1MHPCenterCenter(boolean onRightSide) {
+        // Move forward to Center Bay Wall, while we move the arm into placing position
+        addSequential(new MoveUsingFrontUltrasonic(2));
+        addParallel(new LowGamePieceArmCommand());
+        // This releases the hatch, If we are having trouble lining up we can add
+        // A line follow sequential here
+        addSequential(new CloseClaw());
+        // Move away from the wall
+        addSequential(new MoveUsingFrontUltrasonic(15));
+
+        if(onRightSide) {
+            // Turns us East to the right loading station
+            addSequential(new TurnToCompassHeading(90));
+        } else {
+            // Turns us West to the left loading station
+            addSequential(new TurnToCompassHeading(270));
+        }
+
+        addSequential(new MoveUsingEncoderPID(20));
+        // Turn towards the loading station
+        addSequential(new TurnToCompassHeading(180));
+        // Drive up to the loading station
+        addParallel(new MoveUsingFrontUltrasonic(2));
+        addParallel(new );
 
     }
 
