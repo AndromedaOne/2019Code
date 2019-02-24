0410b3b7f44784374dcbbd70922e157e86632d23
Zach Malstrom <zacharymalstrom@gmail.com>
2019-02-24 14:04:09 -0500

    Adds some group commands and autos

--- src/main/java/frc/robot/groupcommands/autopaths/LoadingStationFromBay3.java
+++ src/main/java/frc/robot/groupcommands/autopaths/LoadingStationFromBay3.java
@@ -4,10 +4,19 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.commands.*;
 
 public class LoadingStationFromBay3 extends CommandGroup {
-
+    /**
+     * Takes the Robot from Bay 3 to the loading station.
+     * The Boolean specifies whether the Robot is on the right or left.
+     * @param onRightSide
+     */
     public LoadingStationFromBay3(boolean onRightSide) {
-
-
+        //TODO: Add non placeholder values.
+        addSequential(new MoveUsingEncoderPID(0));
+        addSequential(new TurnToCompassHeading(180));
+        addSequential(new MoveUsingEncoderPID(0));
+        addSequential(new TurnToFieldCenter());
+        addSequential(new MoveUsingEncoderPID(0));
+        addSequential(new TurnToCompassHeading(180));
+        addSequential(new MoveUsingEncoderPID(0));
     }
-
 }
