e83a3d0c33127d6ca7873282ce3f11884cbc214b
Unknown <sean@ayerdoyles.org>
2019-02-28 11:27:25 -0500

    added functionality to set the setpoint of the PID controller from within the teleop arm command

--- src/main/java/frc/robot/commands/TeleopArm.java
+++ src/main/java/frc/robot/commands/TeleopArm.java
@@ -4,6 +4,9 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 import frc.robot.closedloopcontrollers.MoveArmAndWristSafely;
+import frc.robot.closedloopcontrollers.pidcontrollers.ExtendableArmPIDController;
+import frc.robot.closedloopcontrollers.pidcontrollers.ShoulderPIDController;
+import frc.robot.closedloopcontrollers.pidcontrollers.WristPIDController;
 import frc.robot.subsystems.extendablearmandwrist.ExtendableArmAndWrist;
 import frc.robot.utilities.EnumeratedRawAxis;
 
@@ -13,6 +16,9 @@ public class TeleopArm extends Command {
   ExtendableArmAndWrist extendableArmAndWrist = Robot.extendableArmAndWrist;
   final double sinPi4 = Math.sin(Math.PI / 4);
   final double cosPi4 = Math.cos(Math.PI / 4);
+  private final double WRISTSTICKVALUETODEGREES = 1;
+  private final double SHOULDERSTICKVALUETODEGREES = 1;
+  private final double EXTENSIONSTICKVALUETOINCHES = 1;
 
   public TeleopArm() {
     requires(Robot.extendableArmAndWrist);
@@ -29,16 +35,40 @@ public class TeleopArm extends Command {
     if (Math.abs(shoulderRotateValue) < 0.01) {
       shoulderRotateValue = 0.0;
     }
-    // System.out.println("extensionValue: " + extensionValue);
-    // try {
-    // System.out.println("extensionValue: " + extensionValue);
-    MoveArmAndWristSafely.setTeleopExtensionPower(extensionValue);
-    MoveArmAndWristSafely.setTeleopShoulderPower(shoulderRotateValue);
-    MoveArmAndWristSafely.setTeleopWristPower(wristRotateValue);
-    // } catch (ArmOutOfBoundsException e) {
-    // System.out.println(e.getMessage());
-    // }
+    double topArmEncoderTicks = Robot.topArmExtensionEncoder.getDistanceTicks();
+    double bottomArmEncoderTicks = Robot.bottomArmExtensionEncoder.getDistanceTicks();
 
+    double currentWristPosition = MoveArmAndWristSafely.getWristRotDegrees(topArmEncoderTicks, bottomArmEncoderTicks);
+    double currentExtensionPosition = MoveArmAndWristSafely.getExtensionIn(topArmEncoderTicks, bottomArmEncoderTicks);
+    double currentShoulderPosition = MoveArmAndWristSafely.getShoulderRotDeg(Robot.shoulderEncoder.getDistanceTicks());
+
+    double wristSetpoint = currentWristPosition + wristRotateValue*WRISTSTICKVALUETODEGREES;
+    if(wristSetpoint > MoveArmAndWristSafely.maxWristRotDegrees) {
+      wristSetpoint = MoveArmAndWristSafely.maxWristRotDegrees;
+    }
+    if(wristSetpoint < -MoveArmAndWristSafely.maxWristRotDegrees) {
+      wristSetpoint = -MoveArmAndWristSafely.maxWristRotDegrees;
+    }
+
+    double shoulderSetpoint = currentShoulderPosition + shoulderRotateValue*SHOULDERSTICKVALUETODEGREES;
+    if(shoulderSetpoint > MoveArmAndWristSafely.maxShoulderRotDegrees) {
+      shoulderSetpoint = MoveArmAndWristSafely.maxShoulderRotDegrees;
+    }
+    if(shoulderSetpoint < -MoveArmAndWristSafely.maxShoulderRotDegrees) {
+      shoulderSetpoint = -MoveArmAndWristSafely.maxShoulderRotDegrees;
+    }
+
+    double extensionSetpoint = currentExtensionPosition + extensionValue*EXTENSIONSTICKVALUETOINCHES;
+    if(extensionSetpoint > MoveArmAndWristSafely.maxShoulderRotDegrees) {
+      extensionSetpoint = MoveArmAndWristSafely.maxShoulderRotDegrees;
+    }
+    if(extensionSetpoint < 0) {
+      extensionSetpoint = 0;
+    }
+
+    ExtendableArmPIDController.getInstance().setSetpoint(extensionSetpoint);
+    WristPIDController.getInstance().setSetpoint(wristSetpoint);
+    ShoulderPIDController.getInstance().setSetpoint(shoulderSetpoint);
   }
 
   @Override
