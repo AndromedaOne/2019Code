a6d88bcea2fede8c74a6c150ee98980f3c93c8df
SPD3 <sean@ayerdoyles.org>
2019-03-09 13:39:42 -0500

    TFW you let a mentor access your VSC terminal

--- src/main/java/frc/robot/groupcommands/armwristcommands/PickUpHatchFromGround.java
+++ src/main/java/frc/robot/groupcommands/armwristcommands/PickUpHatchFromGround.java
@@ -1,34 +1,14 @@
 package frc.robot.groupcommands.armwristcommands;
 
-import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.ArmPosition;
-import frc.robot.Robot;
-import frc.robot.commands.armwristcommands.RetractArm;
-import frc.robot.commands.armwristcommands.RotateShoulder;
-import frc.robot.commands.armwristcommands.RotateWrist;
-import frc.robot.utilities.ButtonsEnumerated;
 
-public class PickUpHatchFromGround extends CommandGroup {
-  private final double shoulderPosition = 50;
-  private final double wristPosition = -50;
-  private final double extensionPosition = 28;
+public class PickUpHatchFromGround extends ArmWristMovementCommand {
+  protected static ArmPosition setpoint = new ArmPosition(50, 28, -50);
+  protected static ArmPosition lowerLimit = new ArmPosition(40, 20, -60);
+  protected static ArmPosition upperLimit = new ArmPosition(60, 35, -40);
 
   public PickUpHatchFromGround() {
-    ArmPosition armPosition = Robot.getCurrentArmPosition();
-
-    boolean positiveWrist = (armPosition.getShoulderAngle() + armPosition.getWristAngle()) > 0;
-    boolean sameSidePlacement = ButtonsEnumerated.isPressed(ButtonsEnumerated.LEFTBUMPERBUTTON,
-        Robot.operatorController);
-    double directionFactor = positiveWrist ? 1 : -1;
-    boolean positiveWristDestination = (-wristPosition * directionFactor) > 0 ? true : false;
-    positiveWristDestination = sameSidePlacement ? positiveWristDestination : !positiveWristDestination;
-
-    addSequential(new TuckArm(armPosition.getShoulderAngle(), positiveWristDestination));
-    if (sameSidePlacement) {
-      addSequential(new RotateShoulder(shoulderPosition * directionFactor));
-      addSequential(new RotateWrist(wristPosition * directionFactor));
-      addSequential(new RetractArm(extensionPosition));
-    }
+    super(lowerLimit, upperLimit, setpoint);
   }
 
 }
