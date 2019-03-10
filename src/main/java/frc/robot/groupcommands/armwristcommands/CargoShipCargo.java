4b6adee75c0ff21bfb17fe276e30cc446347fcbc
SPD3 <sean@ayerdoyles.org>
2019-03-10 14:38:55 -0400

    Got rid of unused constants

--- src/main/java/frc/robot/groupcommands/armwristcommands/CargoShipCargo.java
+++ src/main/java/frc/robot/groupcommands/armwristcommands/CargoShipCargo.java
@@ -4,9 +4,6 @@ import frc.robot.ArmPosition;
 import frc.robot.commands.armwristcommands.*;
 
 public class CargoShipCargo extends ArmWristMovementCommand {
-  private final double shoulderPosition = 151;
-  private final double wristPosition = -98.59;
-  private final double extensionPosition = 20.92;
 
   protected static final ArmPosition setpoint = new ArmPosition(151, 20.92, -98.59);
   protected static final ArmPosition lowLimit = new ArmPosition(141, 17.92, -108.59);
--- src/main/java/frc/robot/groupcommands/armwristcommands/LoadingStation.java
+++ src/main/java/frc/robot/groupcommands/armwristcommands/LoadingStation.java
@@ -3,11 +3,6 @@ package frc.robot.groupcommands.armwristcommands;
 import frc.robot.ArmPosition;
 
 public class LoadingStation extends ArmWristMovementCommand {
-
-  private final double shoulderPosition = 11.75;
-  private final double wristPosition = 122.11;
-  private final double extensionPosition = 26.75;
-
   protected static final ArmPosition setpoint = new ArmPosition(11.75, 26.75, 122.11);
   protected static final ArmPosition lowLimit = new ArmPosition(1.75, 23.75, 112.11);
   protected static final ArmPosition highLimit = new ArmPosition(135.16, 3, -46.58);
