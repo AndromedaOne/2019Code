53d3d8cb0512a8997fc592443dd5718f80e0b8fb
Zach Malstrom <zacharymalstrom@gmail.com>
2019-03-23 16:38:29 -0400

    Adds skeleton for code to come for turning.

---
+++ src/main/java/frc/robot/commands/RotateToCenterOfLineSensor.java
@@ -0,0 +1,34 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class RotateToCenterOfLineSensor extends Command{
+    public RotateToCenterOfLineSensor() {
+
+    }
+
+    @Override
+    protected void initialize() {
+        super.initialize();
+    }
+    
+    @Override
+    protected void execute() {
+        super.execute();
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    protected void end() {
+    }
+
+    @Override
+    protected void interrupted() {
+
+    }
+
+}
