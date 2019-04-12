f4556f379dbdac43d1f5384d2fe862a968caec4a
William J. Schilp, PhD <williamschilp@gmail.com>
2019-04-12 09:33:02 -0400

    Refactored the encoderPID to be in terms of inches not ticks

--- src/main/java/frc/robot/closedloopcontrollers/pidcontrollers/DrivetrainEncoderPIDController.java
+++ src/main/java/frc/robot/closedloopcontrollers/pidcontrollers/DrivetrainEncoderPIDController.java
@@ -1,6 +1,8 @@
 package frc.robot.closedloopcontrollers.pidcontrollers;
 
 import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
 import frc.robot.Robot;
 import frc.robot.sensors.magencodersensor.MagEncoderSensor;
 
@@ -8,7 +10,10 @@ public class DrivetrainEncoderPIDController extends PIDControllerBase {
 
   private static DrivetrainEncoderPIDController instance;
   private EncoderPIDOut encoderPIDOut;
+  private EncoderPIDIn encoderPIDSrc;
   private MagEncoderSensor encoder;
+
+  private static final double TICKSTOINCHESRATIO = 0;
 
   /**
    * Sets the encoder, encoderPIDOut, trace, and pidConfiguration variables. Also
@@ -19,15 +24,44 @@ public class DrivetrainEncoderPIDController extends PIDControllerBase {
     super.pForMovingQuickly = 0.0001;
     super.iForMovingQuickly = 0.00001;
     super.dForMovingQuickly = 0;
+
+    super.absoluteToleranceForPreciseMovement = 100;
+    super.pForMovingPrecisely = 0.0001;
+    super.iForMovingPrecisely = 0.00001;
+    super.dForMovingPrecisely = 0;
+
+
     super.subsystemName = "EncoderPIDHeader";
     super.pidName = "EncoderPID";
 
     encoder = Robot.drivetrainLeftRearEncoder;
     encoder.putSensorOnLiveWindow(super.subsystemName, "LeftRearEncoder");
     encoderPIDOut = new EncoderPIDOut();
+    encoderPIDSrc = new EncoderPIDIn();
     pidConfiguration = new PIDConfiguration();
     super.setPIDConfiguration(super.pidConfiguration);
-    super.pidMultiton = PIDMultiton.getInstance(encoder, encoderPIDOut, super.pidConfiguration);
+    super.pidMultiton = PIDMultiton.getInstance(encoderPIDSrc, encoderPIDOut, super.pidConfiguration);
+  }
+
+  private class EncoderPIDIn implements PIDSource {
+
+    
+
+    @Override
+    public void setPIDSourceType(PIDSourceType pidSource) {
+
+    }
+
+    @Override
+    public PIDSourceType getPIDSourceType() {
+      return null;
+    }
+
+    @Override
+    public double pidGet() {
+      return encoder.getDistanceTicks() * TICKSTOINCHESRATIO;
+	}
+
   }
 
   private class EncoderPIDOut implements PIDOutput {
