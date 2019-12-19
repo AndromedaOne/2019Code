0b09ce7849186800803d14cfa890556ac2d983c4
SPD3 <sean@ayerdoyles.org>
2019-12-19 11:23:31 -0500

    took all of the velocity mode stuff for the drivetrain and put it in its own file. Still need to fix some errors in this new class because when velocity mode is set or when percent vbus mode is set an output of 0 is sent to the drive train which is no bueno.

--- src/main/java/frc/robot/subsystems/drivetrain/RealDriveTrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain/RealDriveTrain.java
@@ -7,10 +7,11 @@ import com.typesafe.config.Config;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Robot;
 import frc.robot.commands.TeleOpDrive;
+import frc.robot.subsystems.drivetrain.drivecontrol.DriveControl;
+import frc.robot.subsystems.drivetrain.drivecontrol.DriveControlEnum;
 import frc.robot.telemetries.Trace;
 import frc.robot.telemetries.TracePair;
 
@@ -22,16 +23,17 @@ http://www.ctr-electronics.com/downloads/api/java/html/index.html
  *
  */
 public class RealDriveTrain extends DriveTrain {
-  public static ArbitraryModeWPI_TalonSRX driveTrainLeftMaster;
+  public static WPI_TalonSRX driveTrainLeftMaster;
   public static WPI_TalonSRX driveTrainLeftSlave;
   public static SpeedControllerGroup driveTrainLeftSpeedController;
-  public static ArbitraryModeWPI_TalonSRX driveTrainRightMaster;
+  public static WPI_TalonSRX driveTrainRightMaster;
   public static WPI_TalonSRX driveTrainRightSlave;
   public static SpeedControllerGroup driveTrainRightSpeedController;
-  public static DifferentialDrive differentialDrive;
   public static DoubleSolenoid shifterSolenoid;
   private boolean shifterPresentFlag = false;
   private boolean invertTurning = false;
+
+  private DriveControl m_driveControl;
   // private PowerDistributionPanel pdp = new PowerDistributionPanel();
 
   public boolean getShifterPresentFlag() {
@@ -47,7 +49,7 @@ public class RealDriveTrain extends DriveTrain {
     driveTrainRightMaster = initTalonMaster(driveConf, "right");
     driveTrainRightSlave = initTalonSlave(driveConf, "rightSlave", driveTrainRightMaster,
         driveConf.getBoolean("rightSideInverted"));
-    differentialDrive = new DifferentialDrive(driveTrainLeftMaster, driveTrainRightMaster);
+    
     if (conf.hasPath("subsystems.driveTrain.invertTurning")) {
       invertTurning = conf.getBoolean("subsystems.driveTrain.invertTurning");
     }
@@ -59,12 +61,14 @@ public class RealDriveTrain extends DriveTrain {
           driveConf.getInt("pneumatics.backwardsChannel"));
     }
     if (driveConf.getBoolean("useVelocityMode")) {
-      setVelocityMode();
+      m_driveControl = DriveControl.createDriveControl(DriveControlEnum.VELOCITY, driveTrainLeftMaster, driveTrainRightMaster);
+    }else {
+      m_driveControl = DriveControl.createDriveControl(DriveControlEnum.PERCENTVBUS, driveTrainLeftMaster, driveTrainRightMaster);
     }
   }
 
   private RobotGear currentGear = RobotGear.SLOWHIGHGEAR;
-  private final int kTimeoutMs = 30;
+  public static final int kTimeoutMs = 30;
 
   private double lowGearMaxSpeed = 1;
   private double highGearMaxSpeed = 1;
@@ -76,79 +80,9 @@ public class RealDriveTrain extends DriveTrain {
   private int shifterDelayCounter = 0;
   private int delay = 4;
   private double maxSpeed = lowGearMaxSpeed;
-  private final int kLowGearPIDSlot = 0;
-  private final int kHighGearPIDSlot = 1;
+  public static final int kLowGearPIDSlot = 0;
+  public static final int kHighGearPIDSlot = 1;
   private int slotIdx = 0;
-
-  /*
-   * Implement math according to section 12.4.2 of the TALON SRX Software
-   * Reference manual Rev 1.22 Also inspired by
-   * https://phoenix-documentation.readthedocs.io/en/latest/ch16_ClosedLoop.html#
-   * motion-magic-position-velocity-current-closed-loop-closed-loop
-   */
-
-  /**
-   * Specialization of WPI_TalonSRX
-   * 
-   * The WPI_TalonSRX class implements a set() method to set the speed of the
-   * TalonSRX. Unfortunately, that method <i>always</i> drives the motor in
-   * PercentOutput mode. We need to be able drive the motor in Velocity mode to
-   * use the closed loop velocity mode. So we extend WPI_TalonSRX and provide our
-   * own set() method which allows us to set the control mode (via the newly added
-   * setControlMode() method.)
-   */
-  class ArbitraryModeWPI_TalonSRX extends WPI_TalonSRX {
-    private ControlMode controlMode = ControlMode.PercentOutput;
-    private double speed;
-
-    ArbitraryModeWPI_TalonSRX(int deviceNumber) {
-      super(deviceNumber);
-    }
-
-    /**
-     * Set the control mode to be used when driving the motor
-     * 
-     * @param controlMode Typically ControlMode.PercentOutput or
-     * ControlMode.Velocity
-     */
-    public void setControlMode(ControlMode controlMode) {
-      this.controlMode = controlMode;
-    }
-
-    @Override
-    public void set(double speed) {
-      this.speed = speed;
-      set(controlMode, speed);
-      feed();
-    }
-
-    @Override
-    public double get() {
-      return speed;
-    }
-  }
-
-  public void setVBusMode() {
-    setVBusMode(driveTrainLeftMaster);
-    setVBusMode(driveTrainRightMaster);
-    differentialDrive.setMaxOutput(1.0);
-  }
-
-  private void setVBusMode(ArbitraryModeWPI_TalonSRX talon) {
-    talon.setControlMode(ControlMode.PercentOutput);
-  }
-
-  public void setVelocityMode() {
-    setVelocityMode(driveTrainLeftMaster);
-    setVelocityMode(driveTrainRightMaster);
-    differentialDrive.setMaxOutput(maxSpeed);
-    System.out.println("maxSpeed set to " + maxSpeed);
-  }
-
-  private void setVelocityMode(ArbitraryModeWPI_TalonSRX talon) {
-    talon.setControlMode(ControlMode.Velocity);
-
-  }
 
   @Override
   public void setGear(RobotGear gear) {
@@ -236,33 +170,13 @@ public class RealDriveTrain extends DriveTrain {
   // Inspired by
   // https://github.com/CrossTheRoadElec/Phoenix-Examples-Languages/blob/master/Java/VelocityClosedLoop/src/main/java/frc/robot/Robot.java
   // and
-  private ArbitraryModeWPI_TalonSRX initTalonMaster(Config driveConf, String side) {
-    ArbitraryModeWPI_TalonSRX _talon = new ArbitraryModeWPI_TalonSRX(driveConf.getInt(side + "Master"));
-    final double closedLoopNeutralToMaxSpeedSeconds = 0.0;
+  private WPI_TalonSRX initTalonMaster(Config driveConf, String side) {
+    WPI_TalonSRX _talon = new WPI_TalonSRX(driveConf.getInt(side + "Master"));
 
     /* Factory Default all hardware to prevent unexpected behaviour */
     _talon.configFactoryDefault();
     _talon.setInverted(driveConf.getBoolean(side + "SideInverted"));
     _talon.setSensorPhase(driveConf.getBoolean(side + "SideSensorInverted"));
-
-    /* Config sensor used for Primary PID [Velocity] */
-
-    double lowGearP = 0.0;
-    double lowGearI = 0.0;
-    double lowGearD = 0.0;
-    double highGearP = 0.0;
-    double highGearI = 0.0;
-    double highGearD = 0.0;
-
-    lowGearMaxSpeed = readPIDConfigItem(driveConf, side, "LowGearMaxSpeed", 1);
-    lowGearP = readPIDConfigItem(driveConf, side, "LowGearP", 0);
-    lowGearI = readPIDConfigItem(driveConf, side, "LowGearI", 0);
-    lowGearD = readPIDConfigItem(driveConf, side, "LowGearD", 0);
-
-    highGearMaxSpeed = readPIDConfigItem(driveConf, side, "HighGearMaxSpeed", 1);
-    highGearP = readPIDConfigItem(driveConf, side, "HighGearP", 0);
-    highGearI = readPIDConfigItem(driveConf, side, "HighGearI", 0);
-    highGearD = readPIDConfigItem(driveConf, side, "HighGearD", 0);
 
     _talon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, kTimeoutMs);
     /**
@@ -276,29 +190,7 @@ public class RealDriveTrain extends DriveTrain {
     _talon.configPeakOutputForward(1, kTimeoutMs);
     _talon.configPeakOutputReverse(-1, kTimeoutMs);
 
-    /* Config the Velocity closed loop gains in slot0 */
-    _talon.config_kF(kLowGearPIDSlot, (1023 / lowGearMaxSpeed), kTimeoutMs);
-    _talon.config_kP(kLowGearPIDSlot, lowGearP, kTimeoutMs);
-    _talon.config_kI(kLowGearPIDSlot, lowGearI, kTimeoutMs);
-    _talon.config_kD(kLowGearPIDSlot, lowGearD, kTimeoutMs);
-    _talon.config_IntegralZone(kLowGearPIDSlot, 50, kTimeoutMs);
-    /* Config the Velocity closed loop gains in slot1 */
-    _talon.config_kF(kHighGearPIDSlot, (1023 / highGearMaxSpeed), kTimeoutMs);
-    _talon.config_kP(kHighGearPIDSlot, highGearP, kTimeoutMs);
-    _talon.config_kI(kHighGearPIDSlot, highGearI, kTimeoutMs);
-    _talon.config_kD(kHighGearPIDSlot, highGearD, kTimeoutMs);
-    _talon.config_IntegralZone(kHighGearPIDSlot, 50, kTimeoutMs);
-    _talon.configClosedloopRamp(closedLoopNeutralToMaxSpeedSeconds);
     return _talon;
-  }
-
-  private double readPIDConfigItem(Config driveConf, String side, String configItem, double defaultValue) {
-    double configValue = defaultValue;
-    if (driveConf.hasPath(side + "Side" + configItem)) {
-      configValue = driveConf.getDouble(side + "Side" + configItem);
-    }
-    System.out.println(side + "Side" + configItem + "=" + configValue);
-    return configValue;
   }
 
   // I JUST CHANGED THIS TO STATIC
@@ -347,7 +239,7 @@ public class RealDriveTrain extends DriveTrain {
       currentSpeed = 0;
       currentRotate = 0;
     }
-    differentialDrive.arcadeDrive(currentSpeed, currentRotate * gearMod, squaredInputs);
+    m_driveControl.move(currentSpeed, currentRotate * gearMod, squaredInputs);
     Trace.getInstance().addTrace(false, "move", new TracePair<>("ForwardBack", previousSpeed),
         new TracePair<>("Rotate", rotateAmount));
   }
@@ -369,7 +261,7 @@ public class RealDriveTrain extends DriveTrain {
   }
 
   public void stop() {
-    differentialDrive.stopMotor();
+    m_driveControl.stop();
   }
 
   public WPI_TalonSRX getLeftRearTalon() {
